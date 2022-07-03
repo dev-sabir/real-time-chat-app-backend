2ccfb4c83e7d028169b5424e82a6481a1624e189
sabir9202 <54519614+sabir9202@users.noreply.github.com>
2022-07-03 22:35:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(cors(origin: "http://localhost:3000"));
+app.use(cors({origin: "http://localhost:3000"}));
 app.use(router);
 
 io.on("connect", (socket) => {
